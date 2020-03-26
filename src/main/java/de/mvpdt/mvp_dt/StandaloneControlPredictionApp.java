6bdb1f349927fff4ca5d7f4a98baf0f7c9632d6d
TT <tahsinnnn@yahoo.de>
2020-03-27 00:12:26 +0100

    Added: iterations with for loops over the result of prepareRawDataFromFile() to get timeStamp + value

--- src/main/java/de/mvpdt/mvp_dt/StandaloneControlPredictionApp.java
+++ src/main/java/de/mvpdt/mvp_dt/StandaloneControlPredictionApp.java
@@ -5,6 +5,7 @@ import de.mvpdt.mvp_dt.service.DataPreparationService;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 public class StandaloneControlPredictionApp {
 
@@ -15,7 +16,17 @@ public class StandaloneControlPredictionApp {
                         .getResource("/input/USDJPY_M1_202003190000_202003192016.csv")
                         .getPath(), 60, 2, 3, 4, 5);
 
-        System.out.println(orderedRawDataFromFile.values().iterator().next().toString().replace(',','\n'));
+        // LinkedHashMap<Integer, LinkedHashMap<Integer, LinkedList<LinkedHashMap<Integer, LinkedHashMap<String, Double>>>>>
+        for (Object value : orderedRawDataFromFile.values())
+            for (Object o : ((LinkedHashMap) value).values())
+                for (Object o1 : ((LinkedList) o))
+                    for (Object o2 : ((LinkedHashMap) o1).values())
+                        System.out.println(o2);
+                     // for(Object o3 : ((LinkedHashMap)o2).values())
+                     //    System.out.println(o3);
+
+        // Dieser Statement gibt nur eine Spalte aus (2 - <HIGH>)
+        // System.out.println(orderedRawDataFromFile.values().iterator().next().toString().replace(',','\n'));
 
 
     }
